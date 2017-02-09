0d3cdf1eb7c11e7f69da666fd1245a5fc0d171bf
Ben Mathes <mathes.ben@gmail.com>
2017-02-08 20:49:05 -0800

    simple version without path compression and tests

--- common/models/portCall.js
+++ common/models/portCall.js
@@ -1,33 +1,73 @@
 'use strict';
 
+var _ = require('underscore');
+
 module.exports = function(PortCall) {
 
   PortCall.getRoutes = function(etd, eta, cb) {
-    // For more information on how to query data in loopback please see
-    // https://docs.strongloop.com/display/public/LB/Querying+data
+    // As supplied from haven, null in etd or eta means explicitly
+    // those calls with null in etd/eta respectively. I.e.
+    // When a query asks for "routes w/ ETA after 2012", they actually
+    // will get "routes w/ ETA after 2012 and null etd" but they mean
+    // "routes w/ ETA after 2012 and ANY etd"
+    var andClauses = [];
+    if (etd !== undefined && etd !== null) {
+      andClauses.push({or: [{ etd: { gte: etd } }, { etd: null }]});
+    }
+    if (eta !== undefined && eta !== null) {
+      andClauses.push({or: [{ eta: { lte: eta } }, { eta: null }]});
+    }
+
     const query = {
-      where: {
-        and: [
-          { // port call etd >= etd param, or can be null
-            or: [{ etd: { gte: etd } }, { etd: null }]
-          },
-          { // port call eta <= eta param, or can be null
-            or: [{ eta: { lte: eta } }, { eta: null }]
-          }
-        ]
-      }
+      where: { and: andClauses },
+      // ensures we iterate over the calls in order.
+      // TODO: how does loopback index?
+      order: ['routeId DESC', 'eta ASC' ]
     };
 
     PortCall.find(query)
       .then(calls => {
-        // TODO: convert port calls to voyages/routes
-        console.log(calls);
+        // TODO: timezones? Appears to be read in GMT,
+        // output in local
+        var routeIdToStarts = [];
+        var routes = [];
+        _.each(calls, (endCall, i) => {
+          if (routeIdToStarts[endCall.routeId] == null) {
+            routeIdToStarts[endCall.routeId] = [];
+          }
 
-        return cb(null, calls);
+          // add a route from all previous calls to this destination
+          _.each(routeIdToStarts[endCall.routeId], (startCall) => {
+            // some routes double back, don't add
+            // a route from somewhere to itself.
+            if (startCall.port === endCall.port) { return; }
+
+            routes.push({
+              startPort: startCall.port,
+              endPort: endCall.port,
+              routeId: startCall.routeId,
+              vessel: startCall.vessel,
+              etd: startCall.etd,
+              eta: endCall.eta,
+              // for checking data accuracy
+              startCallId: startCall.id,
+              endCallId: endCall.id
+            });
+          });
+
+          // NOTE: observe edge cases for first/last calls in a route.
+          // first: above loop will have no start. we won't add a route _to_ it.
+          // last: no calls after it, so we won't add a route _from_ it.
+          routeIdToStarts[endCall.routeId].push(endCall);
+        });
+
+        routes = _.sortBy(routes, (route) => {
+          return [parseInt(route.startCallId, 10), parseInt(route.endCallId)];
+        });
+        return cb(null, routes);
       })
       .catch(err => {
         console.log(err);
-
         return cb(err);
       });
   };
--- test/portCall.js
+++ test/portCall.js
@@ -1,9 +1,115 @@
-const assert = require('chai').assert;
+'use strict';
 
-describe('Test', function() {
-  it('should test', function(done) {
-    assert.equal(1 + 1, 2, 'test should pass!');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const _ = require('underscore');
+const server = require('../server/server');
+const assert = chai.assert;
+const should = chai.should();
+const { PortCall } = server.models;
 
-    done();
+chai.use(chaiHttp);
+
+
+// NOTE: in interest of time, using /server/data/portCalls.json as test
+// data scaffolding. Not the perfect test situation, but I haven't learned
+// how to make scaffolding test data with loopback.
+
+// I would much rather specify factories or scaffoldings to isolate
+// test conditions.
+
+
+describe('PortCall', () => {
+  const routesURL = '/api/PortCalls/getRoutes';
+
+  var runTest = (args, cb) => {
+    chai.request(server)
+      .post(routesURL)
+      .send(args)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        cb.apply(null, [err, res]);
+      });
+  };
+
+  // helper function to take server results
+  // and compare to a simplified start/end port name
+  var verifyRoutes = (results, targets) => {
+    results.length.should.equal(targets.length);
+    // iterate from end because we will remove the result
+    // after we find it so it isn't double-counted
+    for (var i=results.length-1; i >= 0; i--) {
+      var result = results[i];
+      for (var j=targets.length-1; j >= 0; j--) {
+        var target = targets[j];
+        if (target[0] === result.startPort && target[1] === result.endPort) {
+          // match: remove the route from the results and the targets
+          results.splice(i,1);
+          targets.splice(j,1);
+          break;
+        }
+      };
+    }
+    results.length.should.equal(0);
+    targets.length.should.equal(0);
+  };
+
+  it('should list ALL routes with no date filters', (done) => {
+    runTest({}, (err, res) => {
+      res.body.length.should.equal(28);
+      done();
+    });
   });
+
+  it('should list routes with only etd filter', (done) => {
+    // filtering down to route 4, everything after LAX
+    runTest({etd: '2016-1-27'}, (err, res) => {
+      verifyRoutes(res.body, [
+        ['USLAX', 'HKHKG'],
+        ['USLAX', 'SGSIN'],
+        ['HKHKG', 'SGSIN'],
+      ]);
+      done();
+    });
+  });
+
+  it('should list routes with etd and eta filter', (done) => {
+    // filtering down to route 4, LAX->HK
+    runTest({etd: '2016-1-27', eta: '2016-1-31'}, (err, res) => {
+      verifyRoutes(res.body, [['USLAX', 'HKHKG']]);
+      done();
+    });
+  });
+
+  it('should not count routes that double back', (done) => {
+    // route 2 doubles back and goes: HK->LA->OAK->SG->HK->LA.
+    // correct to see HK->LA twice, but not HK->HK
+    runTest({etd: '2016-1-04', eta: '2016-1-22'}, (err, res) => {
+      // this etd/eta picks up a bit of route 3 and 1
+      const route2Results = _.filter(res.body, (result) => {
+        return result.routeId === '2';
+      });
+      route2Results.length.should.equal(13);
+      verifyRoutes(route2Results, [
+        ['HKHKG', 'USLAX'],
+        ['HKHKG', 'USOAK'],
+        ['HKHKG', 'SGSIN'],
+        ['HKHKG', 'USLAX'],
+        ['USLAX', 'USOAK'],
+        ['USLAX', 'SGSIN'],
+        ['USLAX', 'HKHKG'],
+        ['USOAK', 'SGSIN'],
+        ['USOAK', 'HKHKG'],
+        ['USOAK', 'USLAX'],
+        ['SGSIN', 'HKHKG'],
+        ['SGSIN', 'USLAX'],
+        ['HKHKG', 'USLAX'],
+      ]);
+      done();
+    });
+  });
+
+
 });
